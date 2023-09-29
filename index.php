2b9b7062465d1c26c29c506d6ab8c467775b83fd
Anıl DEMİR <anil.demir@botas.gov.tr>
2023-09-29 17:06:43 +0300

    search style completed

--- index.php
+++ index.php
@@ -5,19 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="assets/styles/style.css" rel="stylesheet"/>
     <title>Tootle</title>
 </head>
 <body>
-<div class="mainSection">
-    <div class="logoContainer">
-        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-             alt="search image">
-    </div>
-    <div class="searchContainer">
-        <form action="search.php" method="get">
-            <input type="text" class="searchBox" name="term" placeholder="Search">
-            <input type="submit" class="searchButton" value="Search">
-        </form>
+<div class="wrapper indexPage">
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                 alt="search image">
+        </div>
+        <div class="searchContainer">
+            <form action="search.php" method="get">
+                <input type="text" class="searchBox" name="term" placeholder="Search">
+                <input type="submit" class="searchButton" value="Search">
+            </form>
+        </div>
     </div>
 </div>
 </body>
--- search.php
+++ search.php
@@ -1,4 +1,57 @@
 <?php
-if(isset($_GET['term'])){
-    echo  "search term is: " . $_GET["term"];
+if (isset($_GET['term']) && ($_GET['term'] !== "")) {
+    $term = $_GET['term'];
+} else {
+    exit("You must entera search term");
 }
+
+if (isset($_GET['type']) && ($_GET['type'] !== "")) {
+    $type = $_GET['type'];
+} else {
+    $type = "sites";
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/styles/style.css" type="text/css">
+    <title>Search</title>
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <div class="headerContent">
+            <div class="logoContainer">
+                <a href="index.php">
+                    <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                         alt="google image here">
+                </a>
+            </div>
+            <div class="searchContainer">
+                <form action="search.php" method="get">
+                    <div class="searchBarContainer">
+                        <input type="text" class="searchBox" name="term">
+                        <button>Search</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="tabsContainer">
+            <div class="tabList">
+                <li class='<?php echo $type=="sites" ? "active" : "" ?>'>
+                    <a href='<?php echo "search.php?term=$term"; ?>'>Sites</a>
+                </li>
+                <li class='<?php echo $type=="images" ? "active" : "" ?>'>
+                    <a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a>
+                </li>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
